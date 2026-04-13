e2a3d3bb7149aef2a74443000437b13885a7088e
Ernest Gupik <ernestgupik@wp.pl>
2026-04-13 23:17:38 +0200

    Cleaner code

--- src/routes.rs
+++ src/routes.rs
@@ -9,20 +9,12 @@ use std::{collections::HashMap, sync::Arc};
 
 pub async fn index(State(state): State<Arc<AppState>>) -> Result<impl IntoResponse, AppError> {
     let index_template = state.templates.get_template("index.html")?; // .context("Failed to load index.html from disk")?; // .context is optional!
-
     let html = index_template.render(context!())?;
-
     Ok(Html(html))
 }
 
-// #[derive(Deserialize)]
-// pub struct RedirectQuery {
-//     url: Option<String>,
-// }
-
 pub async fn open_redirect(
     State(state): State<Arc<AppState>>,
-    // query: Query<RedirectQuery>,
     params: Query<HashMap<String, String>>,
 ) -> Result<impl IntoResponse, AppError> {
     if let Some(url) = &params.get("url") {
@@ -30,8 +22,6 @@ pub async fn open_redirect(
     }
 
     let tmpl = state.templates.get_template("open-redirect.html")?;
-
     let html = tmpl.render(context!())?;
-
     Ok(Html(html).into_response())
 }
